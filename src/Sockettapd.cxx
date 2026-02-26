35da55a9725f22481caa214e42b15cd829f1d3a9
Carlo Wood <carlo@alinoe.com>
2026-02-26 23:58:59 +0100

    Implement --restart and --foreground (WIP).

--- src/Sockettapd.cxx
+++ src/Sockettapd.cxx
@@ -1,5 +1,6 @@
 #include "sys.h"
 #include "Sockettapd.h"
+#include "utils/AIAlert.h"
 
 Sockettapd::Sockettapd(int argc, char* argv[])
 {
@@ -12,7 +13,31 @@ Sockettapd::~Sockettapd() = default;
 bool Sockettapd::parse_command_line_parameter(std::string_view arg, int argc, char* argv[], int* index)
 {
   DoutEntering(dc::notice, "Sockettapd::parse_command_line_parameter(\"" << arg << "\", " << argc << ", " << debug::print_argv(argv) << ", &" << index << ")");
+
+  if (arg == "--restart")
+  {
+    opt_restart_ = true;
+    return true;
+  }
+
+  if (arg == "--foregound")
+  {
+    opt_foreground_ = true;
+    return true;
+  }
+
+  if (arg == "--projectdir")
+  {
+    ++*index;
+    if (*index >= argc)
+      THROW_ALERT("Missing argument for --projectdir");
+    project_dir_ = argv[*index];
+    Dout(dc::notice, "project_dir_ set to " << project_dir_ << ".");
+    return true;
+  }
+
   return false;
+
 }
 
 //virtual
@@ -24,4 +49,9 @@ void Sockettapd::print_usage_extra(std::ostream& os) const
 std::u8string Sockettapd::application_name() const
 {
   return u8"sockettapd";
+}
+
+//static
+void Sockettapd::goto_background()
+{
 }
--- src/Sockettapd.h
+++ src/Sockettapd.h
@@ -1,18 +1,31 @@
 #pragma once
 
 #include "Application.h"
+#include <filesystem>
 
 // Sockettapd
 //
 // Daemon application entrypoint.
 class Sockettapd final : public Application
 {
+ private:
+  bool opt_foreground_{false};          // Set if --foreground.
+  bool opt_restart_{false};             // Set if --restart.
+  std::filesystem::path project_dir_;   // Argument passed to --projectdir <dir>.
+
  public:
   // Construct and initialize base application state.
   Sockettapd(int argc, char* argv[]);
 
   // Destroy sockettapd object.
   ~Sockettapd();
+
+  // Run as daemon.
+  static void goto_background();
+
+  // Option accessors.
+  bool restart() const { return opt_restart_; }
+  bool foreground() const { return opt_foreground_; }
 
  protected:
   // Parse remountd-specific command line parameters.
